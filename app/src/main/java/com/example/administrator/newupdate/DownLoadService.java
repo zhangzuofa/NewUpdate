6838dcf88f19a6da63c8cb904c1fac6f1c5a3034
Tom <291390214@qq.com>
2017-01-11 21:22:55 +0800

    点

--- app/src/main/java/com/example/administrator/newupdate/DownLoadService.java
+++ app/src/main/java/com/example/administrator/newupdate/DownLoadService.java
@@ -6,20 +6,32 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class DownLoadService extends Service {
-    private DownloadTAsk downloadTAsk;
+    private DownloadTAsk downloadTask;
     private String downloadUrl;
   private DownloadListener downloadListener = new DownloadListener() {
       @Override
       public void onSucess() {
-
+             downloadTask = null;
+          stopForeground(true);
+          getNOtificationManager().notify(1,getNOtification("下载成功",-1));
+          Toast.makeText(DownLoadService.this, "下载成功", Toast.LENGTH_SHORT).show();
       }
 
       @Override
       public void onFailed() {
+          downloadTask = null;
+          stopForeground(true);
+          getNOtificationManager().notify(1,getNOtification("下载失败",-1));
+          Toast.makeText(DownLoadService.this, "下载失败", Toast.LENGTH_SHORT).show();
 
       }
 
@@ -32,12 +44,17 @@ public class DownLoadService extends Service {
 
       @Override
       public void onPaused() {
+          downloadTask = null;
+          Toast.makeText(DownLoadService.this, "暂停", Toast.LENGTH_SHORT).show();
+
 
       }
 
       @Override
       public void onCanceled() {
-
+      downloadTask = null;
+          stopForeground(true);
+          Toast.makeText(DownLoadService.this, "下载取消", Toast.LENGTH_SHORT).show();
       }
   };
     private NotificationManager getNOtificationManager(){
@@ -59,9 +76,47 @@ public class DownLoadService extends Service {
         }
         return builder.build();
     }
+
+    DownloadBinder mBinder = new DownloadBinder();
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+      return mBinder;
+    }
+    class DownloadBinder extends Binder{
+        public void startDownload(String url){
+            if (downloadTask == null){
+                downloadUrl = url;
+                downloadTask  = new DownloadTAsk(downloadListener);
+                downloadTask.execute(downloadUrl);
+                startForeground(1,getNOtification("下载...",0));
+                Toast.makeText(DownLoadService.this, "下载.....", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void pauseDownload(){
+            if (downloadTask != null){
+                downloadTask.pauseDownload();
+            }
+        }
+        public void cancelDownload(){
+            if (downloadTask != null){
+                downloadTask.cancelDownload();
+            }else {
+                if (downloadUrl != null) {
+                    String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+                    String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getParent();
+                    File file = new File(directory + fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                  getNOtificationManager().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownLoadService.this, "取消了", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+        }
     }
 }
