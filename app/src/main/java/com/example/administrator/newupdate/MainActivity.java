5007ace93bca2e0a11c9af6ed00b044e52d3decb
Tom <291390214@qq.com>
2017-01-12 21:42:58 +0800

    注入ButteKnife

--- app/src/main/java/com/example/administrator/newupdate/MainActivity.java
+++ app/src/main/java/com/example/administrator/newupdate/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.administrator.newupdate;
 
-import android.os.AsyncTask;
+import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+   @BindView(R.id.start_btn)
+    Button startBtn;
+    @BindView(R.id.pause_btn)
+    Button pauseBtn;
+    @BindView(R.id.stop_btn)
+    Button stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
